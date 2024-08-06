4553c16ea57da3ab184ecabce9cffab6a467e131
sharmaanku <49897503+sharmaanku@users.noreply.github.com>
2024-08-06 17:31:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ def index():
 def page_two():
     return render_template('page_two.html')
 
+
+
+
+
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
